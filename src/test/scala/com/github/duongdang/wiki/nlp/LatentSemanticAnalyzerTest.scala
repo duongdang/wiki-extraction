6b18d66cecc0d0fcbeb774d5fd2e8bf1d0737c08
Duong DANG <dang.duong@gmail.com>
2016-05-30 22:16:33 +0200

    Add wikitravel example for lsa

--- src/test/scala/com/github/duongdang/wiki/nlp/LatentSemanticAnalyzerTest.scala
+++ src/test/scala/com/github/duongdang/wiki/nlp/LatentSemanticAnalyzerTest.scala
@@ -22,11 +22,27 @@ import org.scalatest.{FunSuite,Matchers}
 @RunWith(classOf[JUnitRunner])
 class LatentSemanticAnalyzerTest extends FunSuite with SharedSparkContext with Matchers {
   val xml_dump = "src/test/resources/nlp/enwiki_sports_music.xml"
+  val xml_wikitravel_dump = "src/test/resources/nlp/enwikitravel_lyon_indian_ocean.xml"
   val stopwords_file = "src/main/resources/nlp/en_stopwords.txt"
 
-  test("create ls analyzer") {
+  test("analyze english wiki") {
     val no_groups = 4
     val lsa = LatentSemanticAnalyzer(sc, xml_dump, stopwords_file, no_groups, 50000)
+
+    println("Singular values: " + lsa.svd.s)
+
+    val topConceptTerms = lsa.topTermsInTopConcepts(no_groups, 10)
+    val topConceptDocs = lsa.topDocsInTopConcepts(no_groups, 10)
+    for ((terms, docs) <- topConceptTerms.zip(topConceptDocs)) {
+      println("Concept terms: " + terms.map(_._1).mkString(", "))
+      println("Concept docs: " + docs.map(_._1).mkString(", "))
+      println()
+    }
+  }
+
+  test("analyze english wikitravel") {
+    val no_groups = 7
+    val lsa = LatentSemanticAnalyzer(sc, xml_wikitravel_dump, stopwords_file, no_groups, 50000)
 
     println("Singular values: " + lsa.svd.s)
 
